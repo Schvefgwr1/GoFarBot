d2a582da35750db9b7d6d7f9d3e51ad7a93ed024
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2024-09-19 05:10:12 +0300

    without dotenv

--- src/main/java/com/example/gofarbot/GoFarBotApplication.java
+++ src/main/java/com/example/gofarbot/GoFarBotApplication.java
@@ -1,6 +1,5 @@
 package com.example.gofarbot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,10 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class GoFarBotApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("MINIO_ACCESS_KEY", dotenv.get("MINIO_ACCESS_KEY"));
-        System.setProperty("MINIO_SECRET_KEY", dotenv.get("MINIO_SECRET_KEY"));
-
         SpringApplication.run(GoFarBotApplication.class, args);
     }
 }
