22a142f019d5b0dbd204608b317e4f9d08d986a0
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2024-10-04 00:05:05 +0300

    update cors

--- src/main/java/com/example/gofarbot/security/config/CorsConfig.java
+++ src/main/java/com/example/gofarbot/security/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Разрешить CORS для всех путей
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3001") // Разрешить только с localhost:3000 и localhost:3001
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002") // Разрешить только с localhost:3000 и localhost:3001
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешить определенные HTTP методы
                         .allowedHeaders("*") // Разрешить любые заголовки
                         .allowCredentials(true); // Разрешить передачу учетных данных (куки)
