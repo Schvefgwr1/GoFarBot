b5fa2042b8ade881728871f13976d5fd7630efe9
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2024-10-05 03:37:43 +0300

    update new logic of files

--- src/main/java/com/example/gofarbot/services/StartupRunner.java
+++ src/main/java/com/example/gofarbot/services/StartupRunner.java
@@ -1,22 +1,23 @@
 package com.example.gofarbot.services;
 
 
-import com.example.gofarbot.config.MinioConfig;
+import com.example.gofarbot.controllers.bot_controllers.MainBotController;
 import com.example.gofarbot.data.ConferenceRepository;
+import com.example.gofarbot.data.FileRepository;
 import com.example.gofarbot.models.Conference;
+import com.example.gofarbot.models.File;
 import com.example.gofarbot.services.notifications.DynamicNotificationService;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import com.example.gofarbot.services.web_services.FileService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -24,11 +25,12 @@ import java.util.List;
 @AllArgsConstructor
 @Slf4j
 public class StartupRunner implements ApplicationRunner {
+    private final Long fatherUserId = 411240604L;
     private final DynamicNotificationService dynamicNotificationService;
     private final ConferenceRepository conferenceRepository;
-
-    private final MinioClient minioClient;
-    private final MinioConfig minioConfig;
+    private final FileRepository fileRepository;
+    private final FileService fileService;
+    private final MainBotController mainBotController;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -38,69 +40,36 @@ public class StartupRunner implements ApplicationRunner {
         }
         log.info("Successful creating of notifications to actual conferences in  database");
 
-//        try {
-//            ClassPathResource resource1 = new ClassPathResource("static/Resume.pdf");
-//            ClassPathResource resource2 = new ClassPathResource("static/TestImage.png");
-//            ClassPathResource resource3 = new ClassPathResource("static/01.png");
-//            ClassPathResource resource4 = new ClassPathResource("static/02.png");
-//            ClassPathResource resource5 = new ClassPathResource("static/03.png");
-//
-//
-//            try (InputStream fileInputStream = resource1.getInputStream()) {
-//                minioClient.putObject(
-//                        PutObjectArgs.builder()
-//                                .bucket(minioConfig.getDocumentsBucket())
-//                                .object("Resume.pdf")
-//                                .stream(fileInputStream, fileInputStream.available(), -1)
-//                                .contentType("application/pdf")
-//                                .build()
-//                );
-//            }
-//            try (InputStream fileInputStream = resource2.getInputStream()) {
-//                minioClient.putObject(
-//                        PutObjectArgs.builder()
-//                                .bucket(minioConfig.getPhotosBucket())
-//                                .object("TestImage.png")
-//                                .stream(fileInputStream, fileInputStream.available(), -1)
-//                                .contentType("application/png")
-//                                .build()
-//                );
-//            }
-//            try (InputStream fileInputStream = resource3.getInputStream()) {
-//                minioClient.putObject(
-//                        PutObjectArgs.builder()
-//                                .bucket(minioConfig.getPhotosBucket())
-//                                .object("01.png")
-//                                .stream(fileInputStream, fileInputStream.available(), -1)
-//                                .contentType("application/png")
-//                                .build()
-//                );
-//            }
-//            try (InputStream fileInputStream = resource4.getInputStream()) {
-//                minioClient.putObject(
-//                        PutObjectArgs.builder()
-//                                .bucket(minioConfig.getPhotosBucket())
-//                                .object("02.png")
-//                                .stream(fileInputStream, fileInputStream.available(), -1)
-//                                .contentType("application/png")
-//                                .build()
-//                );
-//            }
-//            try (InputStream fileInputStream = resource4.getInputStream()) {
-//                minioClient.putObject(
-//                        PutObjectArgs.builder()
-//                                .bucket(minioConfig.getPhotosBucket())
-//                                .object("03.png")
-//                                .stream(fileInputStream, fileInputStream.available(), -1)
-//                                .contentType("application/png")
-//                                .build()
-//                );
-//            }
-//
-//            log.info("Upload successful!");
-//        } catch (Exception e) {
-//            log.error(e.getMessage(), e);
-//            log.error("Upload failed!");
-//        }
+        List<File> files = fileRepository.findAll();
+        for(File file: files) {
+            if(file.getFileId() == null) {
+                try {
+                    String fileId;
+                    if (file.getType() == File.FileType.DOCUMENT) {
+                        Message message = mainBotController.executeDocument(SendDocument.builder()
+                                .chatId(fatherUserId)
+                                .document(fileService.getFile(file.getLink(), file.getType()))
+                                .build()
+                        );
+                        fileId = message.getDocument().getFileId();
+                    }
+                    else {
+                        Message message = mainBotController.executePhoto(SendPhoto.builder()
+                                .chatId(fatherUserId)
+                                .photo(fileService.getFile(file.getLink(), file.getType()))
+                                .build()
+                        );
+                        fileId = message.getPhoto().get(0).getFileId();
+                    }
+                    file.setFileId(fileId);
+                    File saveFile = fileRepository.save(file);
+                    log.info("Successful indexing file: {}", saveFile);
+                } catch (TelegramApiException e) {
+                    log.error("Error of tg API in startupRunner: {}", e.getMessage());
+                } catch (Exception e) {
+                    log.error("Unsupported exception: {}", e.getMessage());
+                }
+            }
+        }
     }
 }
