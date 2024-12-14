3f047fa814c2504f103291439f735a55961e8ed9
Schvefgwr1 <92165301+Schvefgwr1@users.noreply.github.com>
2024-12-14 19:11:59 +0300

    update 14.12

--- src/main/java/com/example/gofarbot/services/bot_services/KeyboardsService.java
+++ src/main/java/com/example/gofarbot/services/bot_services/KeyboardsService.java
@@ -114,7 +114,7 @@ public class KeyboardsService {
     private InlineKeyboardMarkup getStartMessageKeyboardMarkup() {
         // Создаем кнопку
         InlineKeyboardButton button1 = InlineKeyboardButton.builder()
-                .text("Вебинар Италия")
+                .text("Регистрация вебинары Австрия 21 и 22 декабря")
                 .callbackData("registration")
                 .build();
         InlineKeyboardButton button6 = InlineKeyboardButton.builder()
